c636095289db52e3c9fbda347aed5cee9af451a8
tomas.burianek <xburian@mail.muni.cz>
2020-07-04 15:15:33 +0200

    add common functions

---
+++ qiskit_common.py
@@ -0,0 +1,4 @@
+def create_bell_pair(qc, a, b):
+    """Creates a bell pair in qc using qubits a & b"""
+    qc.h(a) # Put qubit a into state |+>
+    qc.cx(a, b) # CNOT with a as control and b as target
